6cbae49d5e20dca320fa61270520cfb76cb64345
sin <damon031@icloud.com>
2022-07-03 23:11:28 -0400

    Delete conversions directory

--- conversions/errors.py
+++
@@ -1,7 +0,0 @@
-
-class ConversionsError(Exception):
-    """Base Error class for conversions cog."""
-
-
-class CoinMarketCapError(ConversionsError):
-    """Error class for coinmarketcap errors."""
